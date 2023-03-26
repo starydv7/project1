cfab01780b21a6ad5364e84b59f4b18a6c96b83d
Pawan Yadav <55851664+starydv7@users.noreply.github.com>
2023-03-27 00:12:38 +0530

    frontend navbar setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './screens/Home';
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Login from './screens/Login';
-
 function App() {
   return (
     <div>
-      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
